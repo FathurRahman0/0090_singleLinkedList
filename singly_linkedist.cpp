0aed8804726f2af294b733c8c4400525f729ff77
FathurRahman0 <rahmanfthr165@gmail.com>
2025-05-08 18:58:18 +0700

    membuat perulangan  duplikasi noMhs tidak diijinkan

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -43,6 +43,20 @@ public:
         Node * previeus = START;
         Node *current = START;
         
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+
 
     }
 
